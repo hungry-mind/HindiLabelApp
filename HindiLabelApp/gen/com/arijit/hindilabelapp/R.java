d25023460b59cea8e34c64f8d080cc1f55e63772
hungry-mind <arijit@fasttrack.org.in>
2016-01-06 12:30:32 +0530

    some changes in app design

--- HindiLabelApp/gen/com/arijit/hindilabelapp/R.java
+++ HindiLabelApp/gen/com/arijit/hindilabelapp/R.java
@@ -1084,6 +1084,7 @@ containing a value of this type.
         public static final int CheckBox2=0x7f05003e;
         public static final int CheckBox3=0x7f05003f;
         public static final int CheckBox4=0x7f050040;
+        public static final int CheckBox5=0x7f050041;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1095,7 +1096,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050041;
+        public static final int action_settings=0x7f050042;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
